c87875336434715ea65149db26aa236e90feca8f
BIneli Herve <binelimanga@gmail.com>
2023-06-30 00:28:57 +0100

    solved document processing in pushImageDescription function

--- pushImageDescription/__init__.py
+++ pushImageDescription/__init__.py
@@ -9,7 +9,7 @@ def main(documents: func.DocumentList, signalRMessages: func.Out[str]) -> str:
     # Récupération de la liste de documents sous forme de chaîne de caractères
     doc_list = []
     for doc in documents:
-        doc_list.append(json.dumps(doc))
+        doc_list.append(json.dumps(doc.to_json()))
 
     # Envoi du document aux clients via SignalR
     signalRMessages.set(json.dumps({
